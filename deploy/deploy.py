eb755734d6e57b0fb6f061529d2eab56d61a1bb3
Grzegorz Swirski <grzegorz.swirski@intel.com>
2016-06-07 12:26:53 +0200

    Adding logger

--- deploy/deploy.py
+++ deploy/deploy.py
@@ -21,9 +21,13 @@ application to Cloud Foundry using manifest file).
 """
 
 import json
+import logging
 
 from app_deployment_helpers import cf_cli
 from app_deployment_helpers import cf_helpers
+
+logging.basicConfig(level=logging.INFO)
+LOGGER = logging.getLogger(__name__)
 
 PARSER = cf_helpers.get_parser("space-shuttle-demo")
 PARSER.add_argument('--path_to_model', type=str,
@@ -35,20 +39,28 @@ ARGS = PARSER.parse_args()
 CF_INFO = cf_helpers.get_info(ARGS)
 cf_cli.login(CF_INFO)
 
+LOGGER.info('Creating required service instances...')
 cf_cli.create_service('influxdb088', 'free', 'space-shuttle-db')
 cf_cli.create_service('zookeeper', 'shared', 'zookeeper')
 cf_cli.create_service('gateway', 'Simple', 'space-shuttle-gateway')
 
+LOGGER.info('Uploading model to HDFS...')
 LOCAL_MODEL_PATH = ARGS.path_to_model if ARGS.path_to_model else 'model.tar'
 HDFS_MODEL_PATH = cf_helpers.upload_to_hdfs(ARGS.api_url, CF_INFO.org,
                                             LOCAL_MODEL_PATH,
                                             'model')
+
+LOGGER.info('Creating scoring engine...')
 cf_cli.create_service('scoring-engine', 'Simple',
                       'space-shuttle-scoring-engine',
                       json.dumps({"TAR_ARCHIVE": HDFS_MODEL_PATH}))
 
 PROJECT_DIR = ARGS.project_dir if ARGS.project_dir else \
     cf_helpers.get_project_dir()
+
+LOGGER.info('Creating artifact package...')
 cf_helpers.prepare_package(work_dir=PROJECT_DIR)
+
+LOGGER.info('Pushing application to Cloud Foundry...')
 cf_helpers.push(work_dir=PROJECT_DIR, options=ARGS.app_name)
 
